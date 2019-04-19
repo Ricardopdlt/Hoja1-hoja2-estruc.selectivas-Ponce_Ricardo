913b0d44332fe4782ff158f90447afa7bf22afd2
Ricardo Ponce <joaqopdlt@gmail.com>
2019-04-19 08:45:36 -0500

    primer commit

---
+++ Ej21 Hoja1.cpp
@@ -0,0 +1,69 @@
+//Ej21 Hoja1
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+using namespace System;
+using namespace std;
+
+
+int main()
+
+{
+	// entrada
+
+	float monto;
+	float montof; //con intereses
+
+	// logica
+
+	cout << "Ingrese el monto del prestamo en $: ";
+	cin >> monto;
+
+
+
+	if (monto > 0)
+
+	{
+
+		if (monto < 5000)
+
+		{
+
+			montof = monto * 1.03;
+
+		}
+
+		else
+
+			if (monto >= 5000 && monto <= 10000)
+
+			{
+
+				montof = monto * 1.02;
+
+			}
+
+			else
+
+			{
+
+				montof = monto * 1.01 ;
+
+			}
+
+		cout << "El monto total a pagar es de $ " << montof;// salida
+
+	}
+
+	else
+
+	{
+
+		cout << "Ingrese un monto mayor a 0";//salida
+
+	}
+
+	_getch();
+}
